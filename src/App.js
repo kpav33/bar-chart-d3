fba888c66876699bfe74dd51e01c2cbd4e254e18
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-06-23 14:15:46 +0200

    Using D3 with React test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss';
+import * as d3 from "d3"
+import BarChart from './BarChart'
+
+import React, {useState, useEffect, useRef} from "react"
+
 
 function App() {
+  /*const [data, setData] = useState([])
+  const ref = useRef()
+
+  console.log(data)
+  console.log(ref)
+
+  const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+  const temperatureData = [ 8, 5, 13, 9, 12 ]
+  d3.select(ref.current)
+        .selectAll("h2")
+        .data(temperatureData)
+        .enter()
+            .append("h2")
+            .text("New Temperature")
+
+  
+  useEffect(() => {
+    fetch(dataUrl)
+      .then(response => response.json())
+      .then(data => {
+        setData(data.data)
+      })
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="panel">
+        <h1>Hello World</h1>
+      </div>
+      <div id="myDiv" ref={ref}></div>
     </div>
-  );
+  );*/
+  return ( <BarChart /> )
+
 }
 
 export default App;
