602917299df412bf727fc8e9865d36aaa321c821
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-06-24 17:57:43 +0200

    Basic graph, 7/14 tests passed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,88 @@
-import './styles.scss';
-import * as d3 from "d3"
-import BarChart from './BarChart'
+import "./styles.scss";
+import {
+  select,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+  scaleBand,
+  scaleTime,
+  max,
+} from "d3";
 
-import React, {useState, useEffect, useRef} from "react"
-
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [years, setYears] = useState([]);
+  const [effort, setEffort] = useState([]);
+  const svgRef = useRef();
+
+  const dataUrl =
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+  console.log(data);
+  console.log(effort);
+  console.log(years);
+
+  useEffect(() => {
+    fetch(dataUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.data.map((value) => value[1]));
+        setYears(data.data.map((value) => value[0]));
+      });
+  }, []);
+
+  useEffect(() => {
+    // console.log(svgRef);
+    const svg = select(svgRef.current).attr("width", 500).attr("height", 500);
+
+    // scaleTime
+    const xScale = scaleBand()
+      .domain(data.map((d, index) => index))
+      .range([0, 500]);
+
+    /*const xScale = scaleLinear()
+      .domain([0, max(data, (d) => d)])
+      .range([0, 500]);*/
+
+    const yScale = scaleLinear()
+      .domain([0, max(data, (d) => d)])
+      .range([500, 0]);
+
+    const xAxis = axisBottom(xScale);
+    svg.select(".x-axis").style("transform", "translateY(500px)").call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg.select(".y-axis").style("transform", "translateX(500px)").call(yAxis);
+
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .attr("x", (d, index) => xScale(index))
+      .attr("y", yScale)
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d, index) => 500 - yScale(d));
+  }, [data]);
+
+  return (
+    <>
+      <h1 id="title">Title</h1>
+      <svg ref={svgRef}>
+        <g className="x-axis" id="x-axis" />
+        <g className="y-axis" id="y-axis" />
+      </svg>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update Data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
+        Filter Data
+      </button>
+    </>
+  );
+
   /*const [data, setData] = useState([])
   const ref = useRef()
 
@@ -38,9 +115,8 @@ function App() {
       </div>
       <div id="myDiv" ref={ref}></div>
     </div>
-  );*/
-  return ( <BarChart /> )
-
+  );
+  return ( <BarChart /> )*/
 }
 
 export default App;
